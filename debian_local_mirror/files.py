2dabc3814b015433f961315de3df597b5f2061e5
Aleksey Knyazev <aknyazev@openwaygroup.com>
2021-04-01 12:21:21 +0300

    master: debian_local_mirror: specific files

--- debian_local_mirror/files.py
+++ debian_local_mirror/files.py
@@ -4,6 +4,17 @@ import posixpath
 import requests
 import shutil
 
+class HttpError(Exception):
+    def __init__(self, code=0, url='', resp=None, text=''):
+        self.code = code
+        self.url = url
+        self.resp = resp
+        self.text = text
+
+    def __str__(self):
+        return self.text + ': Code ' + str(self.code) + ' ' + self.url
+
+
 class RepoFile(object):
     def __init__(self, remote, local, sub, absent_ok = False):
         """
@@ -37,13 +48,50 @@ class RepoFile(object):
         logging.debug("Creating folder: '%s'" % _dirpath)
         os.makedirs(_dirpath)
 
-    def check(self):
+    def check_before(self):
         """
-        Check if remote file content is the same as local,
+        Check if remote file content is the same as local before download,
         to be overriden in derived classes
         """
         logging.debug("Check returns false by default")
         return False
+
+    def check_after(self):
+        """
+        Check if remote file content is the same as local after download,
+        to be overriden in derived classes
+        """
+        logging.debug("Check returns true by default")
+        return True
+
+    def _download_remote(self, remote, local, absent_ok):
+        """
+        Do download a remote file
+        :param remote: remote URL
+        :type remote: str
+        :param local: local path
+        :type local: str
+        :param absent_ok: do not raise an exception of file is absent in remote
+        :type absent_ok: bool
+        """
+        _web = requests.Session()
+        _rsp = _web.get(remote, stream = True)
+
+        if (os.path.exists(local)):
+            os.remove(local)
+
+        if _rsp.status_code != 200:
+            if absent_ok:
+                # remove local file
+                logging.debug("File '%s' not found, removing local copy also" % self._remote)
+                return
+
+            raise HttpError(_rsp.status_code, _rsp.url, _rsp, 'Error making request to server')
+
+        logging.info("'%s' ==> '%s'" % (remote, local))
+        with open(local, 'wb') as _fl:
+            shutil.copyfileobj(_rsp.raw, _fl)
+            _fl.flush()
 
     def synchronize(self):
         """
@@ -51,28 +99,46 @@ class RepoFile(object):
         """
         self.check_create_local_path()
 
-        if self.check():
+        if self.check_before():
             logging.debug("File data is OK, no need to donwload")
             return
-        
-        _web = requests.Session()
-        _rsp = _web.get(self._remote, stream = True)
 
-        if (os.path.exists(self._local)):
-            os.remove(self._local)
+        self._download_remote(self._remote, self._local, self._absent_ok)       
 
-        if _rsp.status_code != 200 and self._absent_ok: #HTTP OK
-            # remove local file
-            logging.debug("File '%s' not found, removing local copy also" % self._remote)
-            return
-
-        logging.info("'%s' ==> '%s'" % (self._remote, self._local))
-        with open(self._local, 'wb') as _fl:
-            shutil.copyfileobj(_rsp.raw, _fl)
-            _fl.flush()
+        if not self.check_after():
+            raise IOError("Downloading faied: '%s'" % self._remote)
 
 class RepoFileRelease(RepoFile):
-    pass
+    def _check_signature(self):
+        """
+        Check gpg signature
+        """
+        return True
+
+    def check_before(self):
+        """
+        Get signature file from remote and verify if local file fulfils it
+        """
+        if not os.path.exists(self._local):
+            return False
+
+        self._download_remote(
+                remote = self._remote + ".gpg",
+                local = self._local + ".gpg",
+                absent_ok = False)
+
+        return self._check_signature()
+
+    def check_after(self):
+        """
+        Signature is to be downloaded already, check only
+        """
+        return self._check_signature()
 
 class RepoFileInRelease(RepoFile):
-    pass
+    def check_after(self):
+        """
+        Check self-signed file after downloading
+        """
+        return True
+

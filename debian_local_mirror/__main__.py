4d6fa2ae5048e49ded2ec3ece3da8a1dff50235f
Alexey Knyazev <a.knyazev@octavianonline.com>
2021-10-12 19:49:47 +0300

    arguments added for resiging and removing in-release files

--- debian_local_mirror/__main__.py
+++ debian_local_mirror/__main__.py
@@ -6,8 +6,14 @@ import os
 from .mirror_processor import MirrorProcessor
 
 _ap = argparse.ArgumentParser(description="Create partail local debian mirror")
-_ap.add_argument("--log-level", dest="log_level", type=int, default=50)
-_ap.add_argument("-c", "--config", dest="config_fl", required=True)
+_ap.add_argument("--log-level", dest="log_level", type=int, default=50, help="Logging level")
+_ap.add_argument("-c", "--config", dest="config_fl", required=True, help="JSON mirror configuration")
+_ap.add_argument("--remove-valid-until", dest="remove_valid_until", default=False, action='store_true',
+        help="Remove Valid-Until limit for Release and InRelease files, resigning with GPG key is necessary")
+_ap.add_argument("--resign-key", dest="resign_key", default=None, 
+        help="Path to private GPG key for resigning Release and InRelease files")
+_ap.add_argument("--key-passphrase", dest="key_passphrase", default=None, 
+        help="Passphrase for GPG key for resigning Release and InRelease files")
 _ag = _ap.parse_args()
 logging.basicConfig(
     format="%(asctime)s: %(levelname)s: %(filename)s: %(funcName)s: %(lineno)d: %(message)s", 
